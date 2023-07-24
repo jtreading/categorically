23dfe1976bef0f893b012bf54c704ce7178ac79c
Jonathon Reading <jonathonreading@gmail.com>
2023-07-24 16:08:21 -0600

    Adds test data seeding

--- Categorically/Program.cs
+++ Categorically/Program.cs
@@ -1,7 +1,9 @@
 using Categorically.Data;
+using Categorically.DataSeeders;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -38,5 +40,50 @@ app.UseRouting();
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
+
+// Seed the database with test data
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDBContext>();
+    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+
+    try
+    {
+        // Clear the database
+        dbContext.Database.EnsureDeleted();
+
+        // Apply any pending migrations
+        dbContext.Database.Migrate();
+
+        // Seed the database with test data
+        if (!dbContext.Users.Any())
+        {
+            var users = UserSeeder.GenerateUsers(20);
+            dbContext.Users.AddRange(users);
+            dbContext.SaveChanges();
+        }
+
+        if (!dbContext.Categories.Any())
+        {
+            var users = dbContext.Users.ToList();
+            var categories = CategorySeeder.GenerateCategories(users);
+            dbContext.Categories.AddRange(categories);
+            dbContext.SaveChanges();
+        }
+
+        if (!dbContext.Transactions.Any())
+        {
+            var users = dbContext.Users.ToList();
+            var categories = dbContext.Categories.ToList();
+            var transactions = TransactionSeeder.GenerateTransactions(users, categories);
+            dbContext.Transactions.AddRange(transactions);
+            dbContext.SaveChanges();
+        }
+    }
+    catch (Exception ex)
+    {
+        logger.LogError(ex, "An error occurred while seeding the database.");
+    }
+}
 
 app.Run();
