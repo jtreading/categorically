84bb0f22098d86aa1860dc39d923ed6191ceb0a9
Jonathon Reading <jonathonreading@gmail.com>
2023-12-10 15:13:47 -0700

    Update AfterInstall script

--- scripts/AfterInstall.ps1
+++ scripts/AfterInstall.ps1
@@ -1,8 +1,16 @@
 # scripts/AfterInstall.ps1
 Write-Host "AfterInstall started."
 
-# Navigate to the installation directory
-cd C:/CategoricallyApp
+# Navigate to the directory where your application is located
+cd "C:\CategoricallyApp"
 
-# Start the Blazor application
-Start-Process "dotnet" -ArgumentList "/publish/Categorically.Web.dll" -NoNewWindow -Wait
+# Define the path to your main application DLL
+$applicationDll = "C:\CategoricallyApp\Categorically.Web.dll"
+
+# Check if the DLL file exists
+if (Test-Path $applicationDll) {
+    # Start the application using dotnet
+    Start-Process "dotnet" -ArgumentList $applicationDll -NoNewWindow -Wait
+} else {
+    Write-Host "Error: Application DLL not found at $applicationDll"
+}
